199361a99035ffe5b22c3d1ac3ae8eb907108471
emkor93 <emkor93@gmail.com>
2018-12-05 21:00:43 +0100

    added test for calculating checksum

--- src/main/java/org/emkor/Puzzle02.java
+++ src/main/java/org/emkor/Puzzle02.java
@@ -1,14 +1,31 @@
 package org.emkor;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static org.emkor.TextFileUtil.readLines;
 
 public class Puzzle02 {
     public static void main(String[] args) {
+        List<String> lines = readLines("/home/mat/projects/aoc18/src/resources/puzzle01.txt");
+
+        Integer checkSum = countChecksum(lines);
+        System.out.println("The checksum is " + checkSum);
+    }
+
+    public static Integer countChecksum(List<String> lines) {
         Integer twoLettersDuplicatedCount = 0;
         Integer threeLettersDuplicatedCount = 0;
-
-        Integer checkSum = twoLettersDuplicatedCount * threeLettersDuplicatedCount;
+        for (String line : lines) {
+            if (hasTwoLetters(line)) {
+                twoLettersDuplicatedCount += 1;
+            }
+            if (hasThreeLetters(line)) {
+                threeLettersDuplicatedCount += 1;
+            }
+        }
+        return (twoLettersDuplicatedCount * threeLettersDuplicatedCount);
     }
 
     static Boolean hasThreeLetters(String code) {
@@ -16,8 +33,7 @@ public class Puzzle02 {
         for (Character c : code.toCharArray()) {
             if (charToCount.containsKey(c)) {
                 charToCount.put(c, charToCount.get(c) + 1);
-            }
-            else {
+            } else {
                 charToCount.put(c, 1);
             }
         }
@@ -29,8 +45,7 @@ public class Puzzle02 {
         for (Character c : code.toCharArray()) {
             if (charToCount.containsKey(c)) {
                 charToCount.put(c, charToCount.get(c) + 1);
-            }
-            else {
+            } else {
                 charToCount.put(c, 1);
             }
         }
--- src/test/java/org/emkor/Puzzle02Test.java
+++ src/test/java/org/emkor/Puzzle02Test.java
@@ -2,8 +2,10 @@ package org.emkor;
 
 import org.junit.Test;
 
-import static org.emkor.Puzzle02.hasThreeLetters;
-import static org.emkor.Puzzle02.hasTwoLetters;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.emkor.Puzzle02.*;
 import static org.junit.Assert.assertEquals;
 
 public class Puzzle02Test {
@@ -28,5 +30,13 @@ public class Puzzle02Test {
         assertEquals(true, hasTwoLetters("aabcdd"));
         assertEquals(true, hasTwoLetters("abcdee"));
         assertEquals(false, hasTwoLetters("ababab"));
+    }
+
+    @Test
+    public void countChecksumTest() {
+        List<String> lines = Arrays.asList("abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab");
+        Integer expectedChecksum = 12;
+        Integer actualChecksum = countChecksum(lines);
+        assertEquals(expectedChecksum, actualChecksum);
     }
 }
